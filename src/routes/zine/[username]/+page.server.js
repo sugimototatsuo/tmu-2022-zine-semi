eb4c02b03e469ebf279bc76c0ea6bef9e1d4cdf0
SUGIMOTO Tatsuo <sugi2000@gmail.com>
2023-08-05 21:37:19 +0900

    update images name

--- src/routes/zine/[username]/+page.server.js
+++ src/routes/zine/[username]/+page.server.js
@@ -3,7 +3,8 @@ import * as d3 from "d3";
 export async function load({ params }) {
   const username = params.username;
   const uri = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQZ9b1A3fT0h_78kcIRpKUMotr2n7xJLamveN0O1usl28dO94DuLZnORVvz4K9eI9KKUYki0aHJnWjL/pub?gid=700951681&single=true&output=tsv";
-  const zines = (await d3.tsv(uri)).slice(1)
+  const zines = (await d3.tsv(uri, d3.autoType))
+    .slice(1)
     .filter(d => d.visibility === '全体公開')
     .filter(d => d.imgs > 0)
 
