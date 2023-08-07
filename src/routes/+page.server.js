143dec0f88f13f7add45d1ad0b29690d64fdb2d0
SUGIMOTO Tatsuo <sugi2000@gmail.com>
2023-08-07 16:18:12 +0900

    ch favicon fix zine list

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,10 +3,9 @@ import * as d3 from "d3";
 export async function load() {
   const uri = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQZ9b1A3fT0h_78kcIRpKUMotr2n7xJLamveN0O1usl28dO94DuLZnORVvz4K9eI9KKUYki0aHJnWjL/pub?gid=700951681&single=true&output=tsv";
   let zines = (await d3.tsv(uri, d3.autoType))
-    .slice(1)
     .sort((a, b) => b.imgs - a.imgs)
     .sort((a, b) => a.visibility.localeCompare(b.visibility));
-  // console.log(groups);
+  console.log(zines.length);
 
   zines = zines.map(d => ({
     ...d,
